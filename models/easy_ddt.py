c5c327c5feb99a096c7090dde8d9b516379de359
gianmarco <gconte@dinamicheaziendali.it>
2018-02-13 11:02:16 +0100

    [ADD&REF] Funzione ddt_get_location leggermente modificata. Inserito controllo che non trova state_id non prova a stamparlo e quindi non va in errore.  Aggiunto Numero ddt a tree picking  Aggiunto controllo su stampa ddt che se non c'è il partner, il report viene stampato nella lingua dell'azienda configurata su odoo.  Aggiunte traduzioni

--- models/easy_ddt.py
+++ models/easy_ddt.py
@@ -101,27 +101,20 @@ class StockPicking(models.Model):
     #     return data
 
     @api.multi
-    def ddt_get_location(self, ddtlocation):
+    def ddt_get_location(self, location_id):
         model_warehouse = self.env['stock.warehouse']
-        model_location = self.env['stock.location']
-        locations = model_location.browse(ddtlocation)
-        location = locations.location_id.id
-        warehouses = model_warehouse.search(
-            [('view_location_id', '=',
-              location),
+        warehouse = model_warehouse.search(
+            [('lot_stock_id', '=',
+              location_id),
              ])
-        if warehouses:
-            for warehouse in warehouses:
-                data = [warehouse.partner_id.name,
-                        '{street}'.format(street=warehouse.partner_id.street),
-                        ('{zip}'.format(zip=warehouse.partner_id.zip) + ' ' +
-                         '{city}'.format(city=warehouse.partner_id.city) + ' ' +
-                         '(' + '{state}'.format(
-                            state=warehouse.partner_id.state_id.name)
-                         + ')'), ]
-                return data
-        else:
-            return ''
+        data = [warehouse.partner_id.id, warehouse.partner_id.name]
+        if warehouse.partner_id:
+            data = [warehouse.partner_id.name,
+                    warehouse.partner_id.street,
+                    (warehouse.partner_id.zip + ' ' +
+                     warehouse.partner_id.city + ' ' +
+                     '(' + warehouse.partner_id.state_id.name + ')' if warehouse.partner_id.state_id else ''), ]
+        return data
 
     @api.multi
     def ddt_time_report(self, time_ddt):
