5029e79759cc7de3d2242c5158d472512061b4c5
Matteo Bilotta <mbilotta@linkeurope.it>
2020-06-12 18:36:18 +0200

    [FIX] Spostata la visualizzazione dei permessi sugli utenti.

--- l10n_it_delivery_note/models/res_config_settings.py
+++ l10n_it_delivery_note/models/res_config_settings.py
@@ -10,9 +10,6 @@ class ResConfigSettings(models.TransientModel):
     group_use_advanced_delivery_notes = fields.Boolean(string=_("Use the advanced delivery notes behaviour"),
                                                        implied_group='l10n_it_delivery_note.'
                                                                      'use_advanced_delivery_notes')
-    group_show_product_related_fields = fields.Boolean(string=_("Show product information in the delivery note lines"),
-                                                       implied_group='l10n_it_delivery_note.'
-                                                                     'show_product_related_fields')
 
     draft_delivery_note_invoicing_notify = fields.Boolean(related='company_id.draft_delivery_note_invoicing_notify',
                                                           readonly=False)
