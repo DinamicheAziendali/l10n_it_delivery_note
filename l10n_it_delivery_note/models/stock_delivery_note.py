40e4170c3b58e3a920914a3c5871977524267952
gborruso@dinamicheaziendali.it <gborruso@dinamicheaziendali.it>
2020-07-16 09:32:49 +0200

    [REF] aggiornate funzioni per Expected singleton

--- l10n_it_delivery_note/models/stock_delivery_note.py
+++ l10n_it_delivery_note/models/stock_delivery_note.py
@@ -288,7 +288,7 @@ class StockDeliveryNote(models.Model):
             #
             # SMELLS: Perché solo quelli 'da fatturare'?
             #
-            sales = self.mapped('picking_ids.sale_id') \
+            sales = note.mapped('picking_ids.sale_id') \
                 .filtered(
                 lambda o: o.invoice_status == DOMAIN_INVOICE_STATUSES[1])
 
@@ -592,11 +592,11 @@ class StockDeliveryNoteLine(models.Model):
 
     sequence = fields.Integer(string="Sequence", required=True, default=10,
                               index=True)
-    name = fields.Text(string="Description", required=True)
+    name = fields.Html(string="Description", required=True)
     display_type = fields.Selection(LINE_DISPLAY_TYPES, string="Line type",
                                     default=False)
     product_id = fields.Many2one('product.product', string="Product")
-    product_description = fields.Text(related='product_id.description_sale')
+    product_description = fields.Html(related='product_id.description_sale')
     product_qty = fields.Float(string="Quantity", digits=dp.get_precision(
         'Product Unit of Measure'), default=1.0)
     product_uom_id = fields.Many2one('uom.uom', string="UoM",
