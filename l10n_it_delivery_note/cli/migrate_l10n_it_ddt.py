434a8abe6c034b9ad79524d56e5fc8cb1d1b6f0a
Matteo Bilotta <mbilotta@linkeurope.it>
2020-10-30 15:50:13 +0100

    [FIX] Closes #30.

--- l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py
+++ l10n_it_delivery_note/cli/migrate_l10n_it_ddt.py
@@ -20,7 +20,7 @@ STATES_MAPPING = {
 
 
 # noinspection PyPep8Naming
-class MigrateL10nItDdt(EasyCommand):
+class Migrate_L10n_It_Ddt(EasyCommand):
     _carriage_conditions = None
     _goods_descriptions = None
     _transportation_reasons = None
@@ -93,7 +93,8 @@ class MigrateL10nItDdt(EasyCommand):
                 "You don't need to run this command."
             ))
 
-        new_sequence = self.env.ref('l10n_it_delivery_note.delivery_note_sequence_ddt')
+        new_sequence = self.env.ref('l10n_it_delivery_note_base.'
+                                    'delivery_note_sequence_ddt')
         if new_sequence.number_next_actual > 1:
             raise ValidationError(_(
                 "It seems that at least one delivery note has been "
@@ -212,7 +213,7 @@ class MigrateL10nItDdt(EasyCommand):
         DeliveryNoteType = self.env['stock.delivery.note.type']
 
         old_type = self.env.ref('l10n_it_ddt.ddt_type_ddt')
-        new_type = self.env.ref('l10n_it_delivery_note.delivery_note_type_ddt')
+        new_type = self.env.ref('l10n_it_delivery_note_base.delivery_note_type_ddt')
         new_type.write({'sequence_id': old_type.sequence_id.id})
 
         self.env.cr.execute("""
@@ -295,7 +296,26 @@ class MigrateL10nItDdt(EasyCommand):
 
         documents = Document.search([], order='id ASC')
         for document in documents:
-            DeliveryNote.create(vals_getter(document))
+            delivery_note = DeliveryNote.create(vals_getter(document))
+            extra_lines = document.line_ids.filtered(lambda l: not l.move_id)
+
+            if extra_lines:
+                lines_vals = []
+
+                for line in extra_lines:
+                    lines_vals.append({
+                        'name': line.name,
+                        'product_id': line.product_id.id,
+                        'product_qty': line.product_uom_qty,
+                        'product_uom_id': line.product_uom_id.id,
+                        'price_unit': line.price_unit,
+                        'discount': line.discount,
+                        'tax_ids': [(4, t.id) for t in line.tax_ids]
+                    })
+
+                delivery_note.write({
+                    'line_ids': [(0, False, vals) for vals in lines_vals]
+                })
 
         _logger.info("Documents data successfully migrated.")
 
